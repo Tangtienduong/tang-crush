81d34c4b7ea0b973de5934c8d7761f15aeb53d14
Tangtienduong <118056910+Tangtienduong@users.noreply.github.com>
2022-11-12 20:00:14 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,20 +1,21 @@
 const CONFIG = {
    titlewed:  "Demo",
    introTitle: 'nguyễn thị ngọc ngân!',
-    introDesc: `Trái đất vốn lạ thường
-    Mà sao em cứ đi nhầm đường
-    Lạc vào tim anh lẻ loi
-    Đằng sau chữ yêu đây là thương`,
-    btnIntro: 'hihi',
+    introDesc: `tao đã tán tỉnh mày 6 năm bị từ chối 
+                 bị từ chối 4 lần nhưng tao vẫn
+                 yêu mày lần đầu thấy mày năm lớp 4
+                  thì tao đã yêu mày mất rồi
+                  ngọc ngân tao yêu mày nhất ❤❤❤`,
+    btnIntro: 'mau chóng làm ny tao đi',
     title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 😙',
     desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
-    btnYes: 'Vẫn cứ là thích anh <33',
-    btnNo: 'Không, Anh trai à :3',
-    question:'Trên thế giới hơn 7 tỉ người mà sao em lại yêu anh <3',
-    btnReply: 'Gửi cho anh <3',
-    reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
-    mess: 'Anh biết mà 🥰. Yêu em nhiều nhiều 😘😘',
-    messDesc: 'Tối nay 7h anh qua đón nhé công chúa.',
+    btnYes: 'yêu tiến dương nhất <33',
+    btnNo: 'tao đã yêu mày lâu lắm rồi :3',
+    question:'Trên thế giới hơn 7 tỉ người mà sao mày lại yêu tao <3',
+    btnReply: 'Gửi cho dương ny của ngân <3',
+    reply: 'Tao cũng yêu mày dương<33333333',
+    mess: 'tao biết mà 🥰. Tao Cũng yêu mày nhất 😘😘',
+    messDesc: 'Mày chính là bạn gái của tao ngọc ngân❤️❤️❤️.',
     btnAccept: 'Okiiiii lun <3',
     messLink: 'http://fb.com' //link mess của các bạn. VD: https://m.me/nam.nodemy
 }
