b420f79760972317cc0450353458683f55b0b772
Tangtienduong <118056910+Tangtienduong@users.noreply.github.com>
2023-10-31 11:06:56 +0700

    Update config.js
    Ny của tui

--- config.js
+++ config.js
@@ -1,21 +1,29 @@
 const CONFIG = {
    titlewed:  "Demo",
-   introTitle: 'nguyễn thị ngọc ngân!',
-    introDesc: `tao đã tán tỉnh mày 6 năm bị từ chối 
-                 bị từ chối 4 lần nhưng tao vẫn
-                 yêu mày lần đầu thấy mày năm lớp 4
-                  thì tao đã yêu mày mất rồi
-                  ngọc ngân tao yêu mày nhất ❤❤❤`,
-    btnIntro: 'mau chóng làm ny tao đi',
+   introTitle: 'Ngô hồng tiến!',
+    introDesc: ` bạn trước màn hình ơi mặc dù tui
+               với bạn ko wen bt j nhưng chúng ta đã
+               yêu nhau đc 3 tháng rồi nên bây giờ bạn
+               là người con gái rất quan trọng đối vs tui
+               tui muốn được chăm sóc và bên cậu cả đời này
+               vì tui đã quá yêu cậu rồi nên hãy hứa vs tui là 
+               dù có chuyện j xảy ra hay nói cho tui bt đừng giấu tui
+               và hãy hứa rằng bạn sẽ mãi bên tui và yêu 1 mình tui thôi
+               và bạn cũng ko đc đi thành phố hay là làm chỗ khác vì chỉ khi
+               ở trong đây tui mới có thể bảo vệ đc bạn có chuyện j thì bạn cứ kêu tui
+               vệ sinh tiếp bạn hay sao sao đó tui đều sẵn sàng làm nên đừng nghĩ làm 
+               mà bỏ tui lại 1 mình nha 😔😔 tui thật sự ko muốn bạn nghĩ ko muốn bạn
+               đi vì tui sợ 1 ngảy nào đó sẽ mất bạn vì tui thực lòng yêu bạn❤❤❤`,
+    btnIntro: ' hãy ở bên tui nhé ',
     title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 😙',
     desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
     btnYes: 'yêu tiến dương nhất <33',
-    btnNo: 'tao đã yêu mày lâu lắm rồi :3',
-    question:'Trên thế giới hơn 7 tỉ người mà sao mày lại yêu tao <3',
-    btnReply: 'Gửi cho dương ny của ngân <3',
-    reply: 'Tao cũng yêu mày dương<33333333',
-    mess: 'tao biết mà 🥰. Tao Cũng yêu mày nhất 😘😘',
-    messDesc: 'Mày chính là bạn gái của tao ngọc ngân❤️❤️❤️.',
+    btnNo: ' tui sẽ yêu bạn mãi mãi :3',
+    question:'Trên thế giới hơn 7 tỉ người mà sao bạn lại yêu tui<3',
+    btnReply: 'Gửi cho dương ny cute của tiến <3',
+    reply: tui cũng yêu bạn nhất ny cute của tui<33333333',
+    mess: 'tui biết mà 🥰. tui Cũng yêu bạn nhất 😘😘',
+    messDesc: ' bạn chính là tất cả của tui cục zàng ❤️❤️❤️.',
     btnAccept: 'Okiiiii lun <3',
     messLink: 'http://fb.com' //link mess của các bạn. VD: https://m.me/nam.nodemy
 }
